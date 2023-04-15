5c772db5a5caf724515f8261be832d9d32889902
ghaerdi <ghaerdi@outlook.com>
2023-04-15 03:02:35 -0400

    fix(dotnet): algorithm not similar to others languages

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,22 +1,22 @@
-﻿using System;
-
-List<int> primes(int n)
+﻿List<int> sieveOfEratosthenes(int n)
 {
+    int limit = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(n)));
     bool[] booleans = new bool[n + 1];
     List<int> primes = new();
-    int limit = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(n)));
 
-
-    for (int p = 2; p * p <= n; p++)
-        if (booleans[p] == false)
-            for (int i = p * p; i <= n; i += p)
-                booleans[i] = true;
+    for (int i = 2; i <= limit; i++)
+        if (!booleans[i])
+            for (int j = i * i; j < n; j += i)
+                booleans[j] = true;
 
     for (int i = 2; i < n; i++)
-        if (booleans[i] == false)
+        if (!booleans[i])
             primes.Add(i);
 
     return primes;
 }
 
-primes(40_000_000);
+List<int> primes = sieveOfEratosthenes(40_000_000);
+Console.WriteLine(primes.Count);
+Console.WriteLine(primes.First());
+Console.WriteLine(primes.Last());
